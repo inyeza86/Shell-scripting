c1ed6e8e20f5023ffc60a8a589ff1d9336b3923d
inyeza <inyeza1986@gmail.com>
2024-12-13 11:47:53 -0600

    2nd commit

--- aws-cli.sh
+++ aws-cli.sh
@@ -3,3 +3,18 @@ aws s3 ls
 
 # list all ec2 instances on my aws account
 aws ec2 describe-instances
+
+# list all iam users on my aws account
+aws iam list-users
+
+# list all lambda functions on my aws account
+aws lambda list-functions
+
+# list all rds instances on my aws account
+aws rds describe-db-instances
+
+# list all s3 buckets on my aws account
+aws s3 ls
+
+# list all sns topics on my aws account
+aws sns list-topics
